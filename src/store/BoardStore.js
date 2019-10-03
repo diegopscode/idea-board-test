5853eddc25c694807d43429ca2ad1a12743cc0b5
Diego Pancera <dpancera@openvistaglobal.com>
2019-10-03 19:29:11 +0100

    Create BoardStore

--- src/store/BoardStore.js
+++ src/store/BoardStore.js
@@ -1,8 +1,23 @@
 import { observable, computed, action } from "mobx"
 
 class BoardStore {
+    @observable board = []
 
-    @observable books = []
+    @action
+    add(idea) {
+        this.board.push(idea)
+    }
+
+    @action
+    remmove(idea) {
+        let index = this.board.findIndex(item => item.id === idea)
+        if(index) this.board.slice(index, 1)
+    }
+    
+    @computed 
+    get boardSize() {
+        return this.board.length
+    }
 }
 
 export default new BoardStore()
